abc5d28fef8de5d16acb3a892725481d7bff17e7
grigorii-peg <grisha.pegushin@gmail.com>
2023-04-21 19:51:48 +0300

    bb

--- PGA_Task_07/Program.cs
+++ PGA_Task_07/Program.cs
@@ -10,6 +10,43 @@ namespace PGA_Task_07
     {
         static void Main(string[] args)
         {
+            int[] mas = new int[10];
+            FillArray(mas);
+            Console.WriteLine("Исходный массив");
+            OutputArray(mas);
+            ShuffleArray(mas);
+            Console.WriteLine("\nПеремешанный массив");
+            OutputArray(mas);
+            Console.ReadKey();
+        }
+
+        static void FillArray(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                array[i] = random.Next(-10, 10);
+            }
+        }
+
+        static void OutputArray(int[] array)
+        {
+            for (int i = 0; i < array.Length; i++)
+            {
+                Console.Write(array[i] + " ");
+            }
+        }
+
+        static void ShuffleArray(int[] array)
+        {
+            Random random = new Random();
+            for (int i = 0; i < array.Length; i++)
+            {
+                int randomItem = random.Next(i);
+                int x = array[randomItem];
+                array[randomItem] = array[i];
+                array[i] = x;
+            }
         }
     }
 }
